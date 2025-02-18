7da4b5daf579fbae97fdf4258199d27bf749287b
tzanarde <zanarde.tiago@gmail.com>
2025-02-18 15:22:29 -0300

    feat: small button component

---
+++ src/components/ui/SmallButton.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+  defineProps<{
+    label: string,
+    type?: string,
+    image: string;
+  }>();
+</script>
+
+<template>
+  <button
+    :type="type || 'text'"
+    class="small-button">
+    <img :src="image" :alt="label">
+  </button>
+</template>
+
+<style>
+.small-button {
+  background-color: #fff;
+  color: #000;
+  width: 30px;
+  height: 30px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.small-button img {
+  width: 20px;
+  height: 20px;
+}
+</style>
