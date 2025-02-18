722197497f46cb1b5916ced4a02a92a650cc44ec
tzanarde <zanarde.tiago@gmail.com>
2025-02-18 12:39:39 -0300

    feat: horizontal divider component

---
+++ src/components/ui/HorizontalDivider.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="horizontal-divider"></div>
+</template>
+
+<style>
+.horizontal-divider {
+  border: 10px;
+  width: 90%;
+  height: 1px;
+  background: rgb(197, 197, 197);
+  margin: 20px 0;
+  border-radius: 4px;
+}
+
+</style>
