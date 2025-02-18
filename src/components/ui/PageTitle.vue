4ef29db4ba169d08a1a26b0049cf7d31c3183842
tzanarde <zanarde.tiago@gmail.com>
2025-02-18 15:41:51 -0300

    feat: page title component

---
+++ src/components/ui/PageTitle.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  defineProps<{ label: string }>();
+</script>
+
+<template>
+  <h1 class="page-title">{{ label }}</h1>
+</template>
+
+<style>
+.page-title {
+  width: 100%;
+  text-align: start;
+  font-size: 20px;
+}
+</style>
