115d5217c9124627dbbdf560cdca189594da810a
tzanarde <zanarde.tiago@gmail.com>
2025-02-18 12:36:53 -0300

    feat: install vitest

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests.ts", // Carrega as configurações de Jest DOM
+  },
 })
