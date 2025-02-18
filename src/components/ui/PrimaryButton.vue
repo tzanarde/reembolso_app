ac26c2426dd3742948b397ec11e8b02aa6f84e8b
tzanarde <zanarde.tiago@gmail.com>
2025-02-18 12:39:13 -0300

    feat: primary button component

---
+++ src/components/ui/PrimaryButton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  defineProps<{ label: string }>();
+</script>
+
+<template>
+  <button class="primary-button">{{ label }}</button>
+</template>
+
+<style>
+.primary-button {
+background-color: rgb(51, 167, 212);
+text-align: center;
+width: 100%;
+height: 40px;
+border-radius: 5px;
+border-radius: 5px;
+border: 0px;
+cursor: pointer;
+font-size: 18px;
+color: #fff;
+}
+</style>
