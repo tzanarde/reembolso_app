587ca7e2463d5945491baa636bf330116855126e
tzanarde <zanarde.tiago@gmail.com>
2025-02-18 12:39:28 -0300

    test: horizontal divider

---
+++ src/tests/components/HorizontalDivider.test.ts
@@ -0,0 +1,19 @@
+import { render } from "@testing-library/vue";
+import { describe, it, expect } from "vitest";
+import HorizontalDivider from "../../components/ui/HorizontalDivider.vue";
+
+describe("HorizontalDivider.vue", () => {
+  it("renders the button with the correct label", () => {
+    render(HorizontalDivider);
+
+    const {container} = render(HorizontalDivider);
+    expect(container.querySelector(".horizontal-divider")).toBeInTheDocument();
+  });
+
+  it("must have the correct CSS class", () => {
+    render(HorizontalDivider);
+
+    const {container} = render(HorizontalDivider);
+    expect(container.querySelector(".horizontal-divider")).toHaveClass("horizontal-divider");
+  });
+});
