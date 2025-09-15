6fd7c798ce0bfff27b80d3e552c85565d7648c28
tzanarde <zanarde.tiago@gmail.com>
2025-09-15 15:36:45 -0300

    test: add not logged in test cases

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -4,21 +4,41 @@ RSpec.describe "Pages", type: :request do
   describe "GET /home" do
     let(:user) { FactoryBot.build_stubbed(:user, :employee) }
 
-    before do
-      sign_in user
-    end
-
     context 'for the page home' do
-      it "returns http success" do
-        get "/pages/home"
-        expect(response).to have_http_status(:success)
+      context 'when the user is logged in' do
+        it "returns http success" do
+          sign_in user
+          get "/pages/home"
+
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context 'when the user is logged out' do
+        it "returns found status code and redirects to login" do
+          get "/pages/home"
+          expect(response).to have_http_status(:found)
+          expect(response).to redirect_to(new_user_session_path)
+        end
       end
     end
 
     context 'for the page user menu' do
-      it "returns http success" do
-        get "/user_menu"
-        expect(response).to have_http_status(:success)
+      context 'when the user is logged in' do
+        it "returns http success" do
+          sign_in user
+
+          get "/user_menu"
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context 'when the user is logged out' do
+        it "returns found status code and redirects to login" do
+          get "/user_menu"
+          expect(response).to have_http_status(:found)
+          expect(response).to redirect_to(new_user_session_path)
+        end
       end
     end
   end
