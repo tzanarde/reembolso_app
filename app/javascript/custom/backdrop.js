e986540a6eae7a3a260d1e7c73790666e41b9b7d
tzanarde <zanarde.tiago@gmail.com>
2025-09-15 17:21:47 -0300

    fix: confirm modal

--- app/javascript/custom/backdrop.js
+++ app/javascript/custom/backdrop.js
@@ -1,15 +1,20 @@
 document.addEventListener("turbo:load", () => {
   const modal = document.getElementById("modal");
   const backdrop = document.getElementById("modal-backdrop");
-  const closeButton = document.getElementById("sign_up_confirm");
 
   if (modal && backdrop) {
     modal.style.display = "block";
     backdrop.style.display = "block";
-
-    closeButton.addEventListener("click", () => {
-      modal.style.display = "none";
-      backdrop.style.display = "none";
-    });
   }
+
+  document.addEventListener("click", (e) => {
+    if (e.target && e.target.id === "sign_up_confirm") {
+      const modal = document.getElementById("modal");
+      const backdrop = document.getElementById("modal-backdrop");
+      if (modal && backdrop) {
+        modal.style.display = "none";
+        backdrop.style.display = "none";
+      }
+    }
+  });
 });
