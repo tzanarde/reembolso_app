6aa6c2df7c1da9064930ade55fe05ed8d55d8a6a
tzanarde <zanarde.tiago@gmail.com>
2025-09-16 11:38:42 -0300

    test: error messages view

---
+++ spec/views/devise/shared/_error_messages.html.haml_spec.rb
@@ -0,0 +1,56 @@
+require "rails_helper"
+
+RSpec.describe "devise/shared/_error_messages.html.haml", type: :view do
+  context 'for the resource errors' do
+    context 'when there is an error' do
+      let!(:user) { build(:user, :manager, email: nil, password: '123456', password_confirmation: '123456') }
+      it "renders the modal" do
+        user.valid?
+
+        render "devise/shared/error_messages", resource: user
+
+        expect(rendered).to include(I18n.t("errors.forms.user_sign_up.email.blank"))
+      end
+    end
+    context 'when there is no error' do
+      let!(:user) { build(:user, :manager, password: '123456', password_confirmation: '123456') }
+      it "does not render the modal" do
+        user.valid?
+
+        render "devise/shared/error_messages", resource: user
+
+        expect(rendered).to be_empty
+      end
+    end
+  end
+
+  context 'for the flash errors' do
+    context 'when there is an error' do
+      context 'for the unauthenticated error' do
+        it "renders the modal" do
+          flash[:alert] = I18n.t('devise.failure.unauthenticated')
+
+          render "devise/shared/error_messages", resource: nil
+
+          expect(rendered).to include(I18n.t('devise.failure.unauthenticated'))
+        end
+      end
+      context 'for the invalid field error' do
+        it "renders the modal" do
+          flash[:alert] = I18n.t('devise.failure.invalid', authentication_keys: 'Email')
+
+          render "devise/shared/error_messages", resource: nil
+
+          expect(rendered).to include(I18n.t("errors.forms.user_sign_in.invalid_credentials"))
+        end
+      end
+    end
+    context 'when there is no error' do
+      it "does not render the modal" do
+        render "devise/shared/error_messages", resource: nil
+
+        expect(rendered).to be_empty
+      end
+    end
+  end
+end
