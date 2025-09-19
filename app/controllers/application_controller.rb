c25378c34e3f1c6a4711b3fcb1169a39f07fc4cc
tzanarde <zanarde.tiago@gmail.com>
2025-09-19 12:47:40 -0300

    feat: check logout for messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    def after_sign_out_path_for(resource_or_scope)
+        new_user_session_path(logout: true)
+    end
+
     protected
 
     def configure_permitted_parameters
