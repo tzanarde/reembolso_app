277a67a2f87c69fae1265ca31040f3c818d9d2d8
tzanarde <zanarde.tiago@gmail.com>
2025-09-19 08:16:27 -0300

    refact: expense factory

--- spec/factories/expenses.rb
+++ spec/factories/expenses.rb
@@ -8,6 +8,38 @@ FactoryBot.define do
     sequence(:location) { |n| "Local #{n}" }
     user
 
+    trait :with_tags do
+      transient do
+        tags_count { 1 }
+      end
+
+      after(:build) do |expense, evaluator|
+        evaluator.tags_count.times do
+          expense.tags << build(:tag)
+        end
+      end
+    end
+
+    trait :with_nf_file do
+      after(:build) do |expense|
+        expense.receipt_nf.attach(
+          io: File.open(Rails.root.join("spec/fixtures/files/receipt_nf.png")),
+          filename: "receipt_nf.png",
+          content_type: "image/png"
+        )
+      end
+    end
+
+    trait :with_card_file do
+      after(:build) do |expense|
+        expense.receipt_card.attach(
+          io: File.open(Rails.root.join("spec/fixtures/files/receipt_card.png")),
+          filename: "receipt_card.png",
+          content_type: "image/png"
+        )
+      end
+    end
+
     trait :pending do
       status { "P" }
     end
