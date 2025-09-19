8b298e6b1fcacc7e5b58be0cec51fd5f89696a38
tzanarde <zanarde.tiago@gmail.com>
2025-09-19 08:25:53 -0300

    refact: match helper

--- spec/support/match_helpers.rb
+++ spec/support/match_helpers.rb
@@ -1,47 +1,79 @@
 # frozen_string_literal: true
 
 module MatchHelpers extend RSpec::SharedContext
-  def fill_form_sign_up(name: nil, email: nil, password: nil, password_confirmation: nil, user_role: nil, manager: nil)
-    fill_in "user_name", with: name unless name.nil?
-    fill_in "user_email", with: email unless email.nil?
-    fill_in "user_password", with: password unless password.nil?
-    fill_in "user_password_confirmation", with: password_confirmation unless password_confirmation.nil?
-    select user_role, from: "role" unless user_role.nil?
-    select manager, from: "manager_user_id" unless manager.nil?
+  def match_expense_fields_index(expenses_returned, expenses_created, manager, employees)
+    expect(expenses_returned.count).to eq(expenses_created.count)
+    expect(expenses_returned).to match_array(expenses_created)
+
+    expenses_returned.each_with_index do |requested_expense, index|
+      expense_created = expenses_created[index]
+
+      expenses_returned.each_with_index do |expense, index|
+        created_expense = expenses_created[index]
+        
+        unless created_expense.tags.nil?
+          expect(expense.tags.count).to eq(created_expense.tags.count)
+          created_expense.tags.each_with_index do |tag, index|
+            expect(expense.tags[index]).to eq(tag)
+          end
+        end
+      end
+    end
   end
 
-  def fill_form_sign_in(email: nil, password: nil)
-    fill_in "user_email", with: email unless email.nil?
-    fill_in "user_password", with: password unless password.nil?
+  def match_expense_fields_show(expense_returned, expense_created, manager, employees)
+    # Attributes check
+    expected_attributes = ["description", "date", "amount", "location", "status"]
+    expect(expected_attributes.all? { |attr| expense_returned.has_attribute?(attr) }).to be true
+
+    # Values check
+    expect(expense_returned.description).to eq(expense_created["description"])
+    expect(expense_returned.date).to eq(expense_created["date"].to_date)
+    expect(expense_returned.amount).to eq(expense_created["amount"].to_f)
+    expect(expense_returned.location).to eq(expense_created["location"])
+    expect(expense_returned.status).to eq(expense_created["status"])
+
+    # Tags check
+    unless expense_created["tag_names"].nil?
+      expect(expenses_returned.tags.count).to eq(expense_created["tag_names"].count)
+      expense_created["tag_names"].each_with_index do |tag, index|
+        expect(expenses_returned.tags[index].description).to eq(tag)
+      end
+    end
+
+    # Receipts check
+    expect(expense_returned.receipt_nf.download).to eq(expense_created.receipt_nf.download)
+    expect(expense_returned.receipt_card.download).to eq(expense_created.receipt_card.download)
   end
 
-  def fill_form_forgot_password(email: nil)
-    fill_in "user_email", with: email unless email.nil?
-  end
+  def match_expense_fields_create(expense_returned:, expense_expected:, manager:, employee:)
+    # Attributes check
+    expected_attributes = ["description", "date", "amount", "location", "status"]
+    expect(expected_attributes.all? { |attr| expense_returned.has_attribute?(attr) }).to be true
 
-  def fill_form_edit_user(name: nil, email: nil, password: nil, password_confirmation: nil, current_password: nil, user_role: nil, manager: nil)
-    fill_in "user_name", with: name
-    fill_in "user_email", with: email
-    fill_in "user_password", with: password
-    fill_in "user_password_confirmation", with: password_confirmation
-    fill_in "user_current_password", with: current_password
-    select user_role, from: "role", match: :first
-    select manager.name, from: "manager" if manager
-  end
+    # Values check
+    expect(expense_returned.description).to eq(expense_expected[:description])
+    expect(expense_returned.date).to eq(expense_expected[:date].to_date)
+    expect(expense_returned.amount).to eq(expense_expected[:amount].to_f)
+    expect(expense_returned.location).to eq(expense_expected[:location])
+    expect(expense_returned.status).to eq(expense_expected[:status])
 
-  def match_user_edited(user:, name: nil, email: nil, password: nil, password_confirmation: nil, current_password: nil, user_role: nil, manager: nil)
-    expect(user.name).to eq(name)
-    expect(user.email).to eq(email)
-    expect(user.password).to eq(password)
-    expect(user.password_confirmation).to eq(password_confirmation)
-    expect(user.role).to eq(user_role)
-  end
+    # Tags check
+    unless expense_expected[:tag_names].nil?
+      expect(expense_returned.tags.count).to eq(expense_expected[:tag_names].count)
+      expense_expected[:tag_names].each_with_index do |tag, index|
+        expect(expense_returned.tags[index].description).to eq(tag)
+      end
+    end
 
-  def fill_form_new_expense(description: nil, date: nil, amount: nil, location: nil, tags: nil)
-    fill_in "description", with: description, id: "description" unless description.nil?
-    fill_in "date", with: date unless date.nil?
-    fill_in "amount", with: amount unless amount.nil?
-    fill_in "location", with: location unless location.nil?
-    fill_in "tags", with: tags unless tags.nil?
+    # Receipts check
+    expect(expense_returned.receipt_nf.filename.to_s).to eq(expense_expected[:receipt_nf].original_filename)
+    expect(expense_returned.receipt_card.filename.to_s).to eq(expense_expected[:receipt_card].original_filename)
+
+    # Employee check
+    expect(expense_returned.user).to eq(employee)
+
+    # Manager check
+    expect(expense_returned.manager_user).to eq(manager)
   end
 end
